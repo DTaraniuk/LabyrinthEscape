82b5ef27b766af3241cd0a14b6b574c6203e75b5
DTaraniuk <dimataraniuk@gmail.com>
2023-06-20 21:38:48 +0200

    tinkering

--- networking/network_constants.py
+++ networking/network_constants.py
@@ -1,3 +1,3 @@
 from common import constants
 
-GSC_CHANGE_RATE = constants.FPS//0.2  # the number represents frequency in times/sec
+GSC_CHANGE_RATE = constants.FPS//6  # the number represents frequency in times/sec
